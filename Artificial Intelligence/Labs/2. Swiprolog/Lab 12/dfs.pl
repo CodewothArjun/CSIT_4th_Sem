f5727ee260be999e153ad48b11ae4ea57d8df1a7
codewotharjun <dolkhaliarjun9800@gmail.com>
2024-09-15 19:35:04 +0545

    Labs

--- Artificial Intelligence/Labs/2. Swiprolog/Lab 12/dfs.pl
+++ Artificial Intelligence/Labs/2. Swiprolog/Lab 12/dfs.pl
@@ -1,25 +1,28 @@
-go(Start, Goal) :-
-	empty_stack(Empty_been_list),
-	stack(Start, Empty_been_list, Been_list),
-	path(Start, Goal, Been_list).
-	
-	% path implements a depth first search in PROLOG
-	
-	% Current state = goal, print out been list
-path(Goal, Goal, Been_list) :-
-	reverse_print_stack(Been_list).
-	
-path(State, Goal, Been_list) :-
-	mov(State, Next),
-	% not(unsafe(Next)),
-	not(member_stack(Next, Been_list)),
-	stack(Next, Been_list, New_been_list),
-	path(Next, Goal, New_been_list), !.
-	
-reverse_print_stack(S) :-
-	empty_stack(S).
-reverse_print_stack(S) :-
-	stack(E, Rest, S),
-	reverse_print_stack(Rest),
-	write(E), nl.
-  
+% Graph representation
+s(a, b).
+s(a, c).
+s(b, d).
+s(b, e).
+s(c, f).
+s(c, g).
+s(d, h).
+s(e, i).
+s(e, j).
+s(f, k).
+% Goal nodes
+goal(f).
+goal(j).
+% Check if an element is a member of a list
+member(X, [X|_]).
+member(X, [_|Tail]) :- member(X, Tail).
+% Solve using depth-first search
+solve(Node, Solution) :-
+    depthfirst([], Node, Solution).
+% If the current node is a goal, return the solution
+depthfirst(Path, Node, [Node|Path]) :-
+    goal(Node).
+% Explore the next node if the current one is not a goal
+depthfirst(Path, Node, Solution) :-
+    s(Node, Node1),
+    not(member(Node1, Path)),
+    depthfirst([Node|Path], Node1, Solution).
